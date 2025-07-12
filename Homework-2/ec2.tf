6c123cb0fb571751b6eaf86af76a1c03e95601e0
alvindesagun@gmail.com <Alvin De Sagun>
2025-07-12 19:44:20 +0000

    hw2 update - fetch ami id

--- Homework-2/ec2.tf
+++ Homework-2/ec2.tf
@@ -1,9 +1,9 @@
 # create EC2 instance in us-east-1 
 resource "aws_instance" "web" {
-  depends_on    = [aws_key_pair.bastion_key]
-  count         = 3
-  ami           = "ami-020cba7c55df1f615"
-  instance_type = "t2.micro"
+  depends_on             = [aws_key_pair.bastion_key]
+  count                  = 3
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
   subnet_id              = [for s in data.aws_subnet.public : s.id][count.index]
   key_name               = aws_key_pair.bastion_key.key_name
   vpc_security_group_ids = [aws_security_group.allow-hw2-sg.id]
--- Homework-2/main.tf
+++ Homework-2/main.tf
@@ -22,5 +22,14 @@ data "aws_subnet" "public" {
   id       = each.value
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical's AWS account ID
 
+  filter {
+    name   = "name"
+    values = ["*ubuntu-*-22.04-amd64-server-*"]
+  }
+
+}
 
